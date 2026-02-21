df55434c2e6ef26642b4423359cb4ceb20f6b5c8
Barnabas Affonshike <117881010+Barnabas001@users.noreply.github.com>
2026-02-21 10:04:34 +0100

    input correct header adrres

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import Container from "../ui/Container";
-import Header from "../Header";
+import Header from "../layout/Header";
 import TopSection from "../features/TopSection";
 import SidebarNews from "../features/SidebarNews";
 import ArticleList from "../features/ArticleList";
