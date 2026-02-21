df985ce1a8ea42f299c62f04654c80428d650230
Barnabas Affonshike <117881010+Barnabas001@users.noreply.github.com>
2026-02-21 09:34:04 +0100

    build home page

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import Container from "../ui/Container";
+import Header from "../Header";
+import TopSection from "../features/TopSection";
+import SidebarNews from "../features/SidebarNews";
+import ArticleList from "../features/ArticleList";
+
+export default function Home() {
+  return (
+    <Container>
+      <Header />
+
+      <main className="mainGrid">
+        <div>
+          <TopSection />
+          <ArticleList />
+        </div>
+
+        <SidebarNews />
+      </main>
+    </Container>
+  );
+}
